20c5dc0eeb6c196373efe94ea57df27d7caca66c
akrem gomri <gomriakrem1@gmail.com>
2024-05-14 14:45:19 +0200

    first-commit:api-call-to-segment_everything-in-segment_image-file

--- main.py
+++ main.py
@@ -1,68 +1,33 @@
+from fastapi import FastAPI, File, UploadFile
 import os
-
-import numpy as np
-import torch
-from mobile_sam import SamAutomaticMaskGenerator, SamPredictor, sam_model_registry
 from PIL import Image
+import io
+from fastapi.responses import Response, StreamingResponse
+import subprocess
 
-from tools import box_prompt, format_results, point_prompt, fast_process
+# from predict import segment_everything
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+app = FastAPI()
 
-sam_checkpoint = "./mobile_sam.pt"
-model_type = "vit_t"
+@app.post("/segment-image/")
+async def segment_image(file: UploadFile):
 
-mobile_sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-mobile_sam = mobile_sam.to(device=device)
-mobile_sam.eval()
+    # Reading the image and saving it in `resources` folder
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
 
-mask_generator = SamAutomaticMaskGenerator(mobile_sam)
-predictor = SamPredictor(mobile_sam)
+    image_path = "resources/uploaded_image.jpg"
+    os.makedirs(os.path.dirname(image_path), exist_ok=True)
 
-@torch.no_grad()
-def segment_everything(
-    image,
-    input_size=1024,
-    better_quality=False,
-    withContours=True,
-    use_retina=True,
-    mask_random_color=True,
-):
-    global mask_generator
+    image.save(image_path)
 
-    input_size = int(input_size)
-    w, h = image.size
-    scale = input_size / max(w, h)
-    new_w = int(w * scale)
-    new_h = int(h * scale)
-    image = image.resize((new_w, new_h))
-
-    nd_image = np.array(image)
-    annotations = mask_generator.generate(nd_image)
-
-    fig = fast_process(
-        annotations=annotations,
-        image=image,
-        device=device,
-        scale=(1024 // input_size),
-        better_quality=better_quality,
-        mask_random_color=mask_random_color,
-        bbox=None,
-        use_retina=use_retina,
-        withContours=withContours,
-    )
-    return fig
-
-
-
-if __name__ == "__main__":
-    input_path = "resources/dog.jpg"
+    # Processing the image and then saving it to `generated` folder
     output_path = "generated/output.png"
-
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-    image = Image.open(input_path).convert("RGB")
-    fig = segment_everything(
-        image=image
-    )
-    fig.save(output_path)
+    subprocess.run(["venv\\Scripts\\activate", "&&", "python", "segment_image.py", image_path, output_path], shell=True)
+
+    with open(output_path, "rb") as f:
+        image_bytes = f.read()
+
+    return Response(content=image_bytes, media_type="image/png")
